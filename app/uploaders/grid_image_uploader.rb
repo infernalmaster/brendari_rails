77d6c4c6cf7ec07b9af7a7e4676e3c360bf8a9b7
Rostyslav Diachok <infernalmaster1@gmail.com>
2019-06-24 02:42:31 +0300

    allow png

--- app/uploaders/grid_image_uploader.rb
+++ app/uploaders/grid_image_uploader.rb
@@ -48,7 +48,7 @@ class GridImageUploader < CarrierWave::Uploader::Base
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
   def extension_whitelist
-    %w(jpg jpeg)
+    %w(jpg jpeg png)
   end
 
   # Override the filename of the uploaded files:
